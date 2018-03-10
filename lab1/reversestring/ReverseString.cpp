3b951e2410a2dab7488ca83ea2b95dfe868176bd
lukaszkluska <asd@asd.asd>
2018-03-10 14:39:50 +0100

    napisano_reversestring

--- lab1/reversestring/ReverseString.cpp
+++ lab1/reversestring/ReverseString.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "ReverseString.h"
+#include <string>
+
+std::string reverse(std::string str){
+    int dl = str.size();
+    if(dl == 0)return "\0";
+    std::string character = "";
+    character = character + str[dl-1];
+    str.erase(dl-1);
+    return character+reverse(str);
+}
--- lab1/reversestring/ReverseString.h
+++ lab1/reversestring/ReverseString.h
@@ -4,11 +4,9 @@
 
 #ifndef JIMP_EXERCISES_REVERSESTRING_H
 #define JIMP_EXERCISES_REVERSESTRING_H
+#include <string>
 
-
-class ReverseString {
-
-};
+std::string reverse(std::string str);
 
 
 #endif //JIMP_EXERCISES_REVERSESTRING_H
