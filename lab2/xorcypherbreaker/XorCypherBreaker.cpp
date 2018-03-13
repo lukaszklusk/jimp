09a1afaff84da36977acbbf5c132fe3c8de9fa04
lukaszkluska <kluch@Kluchtop.asd>
2018-03-13 22:44:28 +0100

    xorcypherbreaker update

--- lab2/xorcypherbreaker/XorCypherBreaker.cpp
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -6,15 +6,17 @@
 #include <vector>
 #include <algorithm>
 
+using std::find;
+
 std::string XorCypherBreaker(const std::vector<char> &cryptogram, int key_length, const std::vector<std::string> &dictionary){
-    std::string key_guess = "";
+    std::string key_guess, key_match;
     std::vector<std::string> tekst;
     tekst.emplace_back("");
-    bool found = false;
-    int it = 0, it_t = 0;
+    bool done = false;
+    int it = 0, it_t = 0, match = 0, match_max = 0;
     char lit;
     key_guess = "aaa";
-    while(!found){
+    while(!done){
         lit = (char)(cryptogram[it] ^ key_guess[it%3]);
         if((lit >= 'a' && lit <= 'z' ) || (lit >= 'A' && lit <= 'Z')){
             tekst[it_t].push_back(lit);
@@ -27,31 +29,36 @@ std::string XorCypherBreaker(const std::vector<char> &cryptogram, int key_length
         it++;
         if(it == cryptogram.size()){
             while(!tekst.empty()){
-                if(dictionary.end() != std::find(dictionary.begin() , dictionary.end() , tekst.pop_back())){ //nie wiem czemu tu jest zle, kod powinien dzialac
-                    found = true;
+                if(dictionary.end() != find(dictionary.begin() , dictionary.end() , tekst.back())){
+                    match++;
                 }
+                tekst.pop_back();
             }
-            if(!found){
-                if(key_guess[2] == 'z'){
-                    if(key_guess[1] == 'z'){
-                        if(key_guess[0] == 'z'){
-                            break;
-                        }else{
-                            key_guess[0] = (char)((int)key_guess[0] + 1);
-                        }
-                        key_guess[1] = 'a';
+            if(match > match_max){
+                key_match = key_guess;
+                match_max = match;
+            }
+            match = 0;
+            if(key_guess[2] == 'z'){
+                if(key_guess[1] == 'z'){
+                    if(key_guess[0] == 'z'){
+                        done = true;
                     }else{
-                        key_guess[1] = (char)((int)key_guess[1] + 1);
+                        key_guess[0] = (char)((int)key_guess[0] + 1);
                     }
-                    key_guess[2] = 'a';
+                    key_guess[1] = 'a';
                 }else{
-                    key_guess[2] = (char)((int)key_guess[2] + 1);
+                    key_guess[1] = (char)((int)key_guess[1] + 1);
                 }
+                key_guess[2] = 'a';
+            }else{
+                key_guess[2] = (char)((int)key_guess[2] + 1);
             }
             it = 0;
             tekst.clear();
             it_t = 0;
+            tekst.emplace_back("");
         }
     }
-    return key_guess;
+    return key_match;
 }
